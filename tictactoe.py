bda004271a03358d0d245e4ee93c10b2b5decac8
clement7903 <67403465+clement7903@users.noreply.github.com>
2022-01-05 15:28:34 +0800

    Completed player, actions, results functions

--- tictactoe.py
+++ tictactoe.py
@@ -3,6 +3,7 @@ Tic Tac Toe Player
 """
 
 import math
+import copy
 
 X = "X"
 O = "O"
@@ -22,20 +23,60 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-    raise NotImplementedError
+    if board == initial_state():
+        return X
+    
+    x_count = 0
+    o_count = 0
+    for row in board:
+        x_count += row.count(X)
+        o_count += row.count(O)
+
+    if x_count == o_count:
+        return X
+    else:
+        return O
+
+
 
 
 def actions(board):
     """
     Returns set of all possible actions (i, j) available on the board.
     """
-    raise NotImplementedError
+    possible_actions = set()
+
+    for i in range (3):
+        for j in range (3):
+            if board[i][j] == EMPTY:
+                possible_actions.add((i,j))
+    if len(possible_actions) == 0:
+        return 0
+    else:
+        return possible_actions
+
+
 
 
 def result(board, action):
     """
     Returns the board that results from making move (i, j) on the board.
     """
+
+    new_board = copy.deepcopy(board)
+    try: 
+        if new_board[[action[0]][action[1]]] == EMPTY:
+            new_board[[action[0]][action[1]]] = player(new_board)
+        else: 
+            raise Exception
+    except Exception:
+        print("Non-valid action: Spot occupied")
+        
+    return new_board
+
+
+
+
     raise NotImplementedError
 
 
